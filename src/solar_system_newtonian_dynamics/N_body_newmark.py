65362f0d1ac7a2214e7753e98f1cb977c98ae09b
Rémi Capillon <rcapillon@yahoo.fr>
2025-08-16 13:31:35 +0200

    work in progress

--- src/solar_system_newtonian_dynamics/N_body_newmark.py
+++ src/solar_system_newtonian_dynamics/N_body_newmark.py
@@ -1,5 +1,27 @@
 import numpy as np
 
 
-def N_body_newmark(starting_positions, delta_t, n_t):
+G = 6.67408e-11
+
+
+def compute_N_body_forces(positions, masses):
+    """"""
+    n_body = positions.shape[0]
+    mat_forces = np.zeros((n_body, 3))
+
+    vec_radius = np.sqrt(np.sum(np.power(positions, 2), axis=-1))
+
+    for i in range(n_body):
+        radius_i = vec_radius[i]
+        position_i = positions[i, :]
+        mass_i = masses[i]
+        for j in range(i + 1, n_body):
+            radius_j = vec_radius[j]
+            position_j = positions[j, :]
+            mass_j = masses[j]
+
+    return mat_forces
+
+
+def N_body_newmark(starting_positions, masses, delta_t, n_t):
     """"""
