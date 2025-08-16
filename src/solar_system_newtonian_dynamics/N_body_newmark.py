fd4218062dcab57b86f98429ffa8f8e4aa7a0e09
Rémi Capillon <rcapillon@yahoo.fr>
2025-08-16 15:19:30 +0200

    work in progress

--- src/solar_system_newtonian_dynamics/N_body_newmark.py
+++ src/solar_system_newtonian_dynamics/N_body_newmark.py
@@ -5,20 +5,24 @@ G = 6.67408e-11
 
 
 def compute_N_body_forces(positions, masses):
-    """"""
+    """
+    Computes gravitational forces exerted on each body
+    """
     n_body = positions.shape[0]
     mat_forces = np.zeros((n_body, 3))
 
-    vec_radius = np.sqrt(np.sum(np.power(positions, 2), axis=-1))
-
     for i in range(n_body):
-        radius_i = vec_radius[i]
         position_i = positions[i, :]
         mass_i = masses[i]
         for j in range(i + 1, n_body):
-            radius_j = vec_radius[j]
             position_j = positions[j, :]
             mass_j = masses[j]
+
+            distance_i_j = np.linalg.norm(position_j - position_i)
+            vec_r_i_to_j = (position_j - position_i) / distance_i_j
+            F_j_on_i = (G * mass_i * mass_j / distance_i_j ** 2) * vec_r_i_to_j
+            mat_forces[i, :] += F_j_on_i
+            mat_forces[j, :] -= F_j_on_i
 
     return mat_forces
 
